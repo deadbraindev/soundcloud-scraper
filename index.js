f4df054335b06f870986bba8292d9970a248b276
deadbrain <deadbrain@deadbrain-macbook.local>
2021-11-14 00:10:00 +0100

    working multisource downloading and tags writing

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const NodeID3 = require('node-id3');
 const SoundCloud = require("soundcloud-scraper");
 const client = new SoundCloud.Client();
 const fs = require("fs");
+const path = require('path');
+
+const request = require('request'); //pobieranie jpg
 
 
 const app = express();
@@ -12,10 +15,96 @@ const port = 3000;
 //set templating engine
 app.set('view engine', 'ejs');
 
+//dodanie folderu publicznego z js, css itd.
+app.use("/public", express.static('public'));
+
 // app.use(express.urlencoded({ extended: false }));
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
+// function getMusicInfo(songUrl, arr) {
+//   client.getSongInfo(songUrl)
+//   .then(songInfo => {
+//     arr.push(
+//       {
+//       title: songInfo.title,
+//       artist: songInfo.author.name,
+//       album: songInfo.author.url,
+//       composer: "",
+//       genre: "",
+//       subtitle: songInfo.url,
+//       image: songInfo.thumbnail,
+//     })
+//     console.log("arr push")
+//   })
+//   .catch(console.error);
+// };
+
+// async function getSongs1(arrSongs, arrSourcelinks) {
+//   const promises = await arrSourcelinks.map(async item => {
+//     const numItem = new Promise((resolve, reject) => {
+//       getMusicInfo(item, arrSongs);
+//       resolve(item);
+//     });
+//     return numItem;
+//     })
+//     const numItems = await Promise.all(promises);
+//     console.log(numItems + "aaa");
+    
+//     console.log("end map");
+// }
+
+
+async function getSongs(arrSongs, arrSourcelinks) {  
+  for await (const [i, sourcelink] of arrSourcelinks.entries()) {
+    await client.getSongInfo(sourcelink)
+    .then(async songInfo => {
+    await arrSongs.push(
+      {
+      artist: songInfo.author.name,
+      title: songInfo.title,
+      album: songInfo.author.url,
+      composer: "",
+      genre: "",
+      subtitle: songInfo.url,
+      image: songInfo.thumbnail,
+    })
+
+    console.log(i+1 + "/" + arrSourcelinks.length)
+
+
+    })
+    .catch(console.error);
+  }
+}
+
+async function updateSongsTags(arrSongs, arrEdited) {
+  if (typeof arrEdited.artist === "array") {
+    for await (const [i, song] of arrSongs.entries()) {
+      song.artist = arrEdited.artist[i];
+      song.title = arrEdited.title[i];
+      song.album = arrEdited.soundcloud[i];
+      song.composer = arrEdited.spotify[i];
+      song.genre = arrEdited.youtube[i];
+      song.subtitle = arrEdited.sourcelink[i];
+    }
+  }
+  else {
+    arrSongs.artist = arrEdited.artist;
+    arrSongs.title = arrEdited.title;
+    arrSongs.album = arrEdited.soundcloud;
+    arrSongs.composer = arrEdited.spotify;
+    arrSongs.genre = arrEdited.youtube;
+    arrSongs.subtitle = arrEdited.sourcelink;
+  }
+  
+}
+
+
+let songsTags = new Array();
+
+
+//get
 app.get('/', (req, res) => {
 
   res.render('index');
@@ -24,56 +113,96 @@ app.get('/', (req, res) => {
 app.get('/queue', (req, res) => {
 
   res.render('queue');
-  
 });
 
 
-let globalSongInfo = {};
-let globalSourcelink = "";
 
+
+let sourcelinks;
+
+
+
+//post
 app.post('/queue', urlencodedParser, (req, res) => {
 
-  globalSourcelink = req.body.sourcelink; //wyciagniecie linka z routa wczesniej, zeby moc uruchomic scraper w /download
-  client.getSongInfo(globalSourcelink)
-    .then(async song => {
-        console.log(song);
-        globalSongInfo = song;  //jakby wyciagniecie danych song wyzej, globalnie do globalSongInfo, zeby moc je uzyc w pozniejszym roucie
-        res.render('queue', {song: globalSongInfo}); //render strony, z przekazaniem danych, zeby moc je podstawic w inputach
+    if(typeof req.body.sourcelink === "string") {
+      sourcelinks = new Array(req.body.sourcelink);
+
+    }
+    else {
+      sourcelinks = req.body.sourcelink;
+
+    }
+ 
+  async function rend() {
+    await getSongs(songsTags, sourcelinks);
+    res.render('queue', {songs: songsTags});
+  }
+  // const tags = NodeID3.read(`./music/muzykolega - Always i recognize You.mp3`)
+  // console.log(tags);
+
+  rend();
+});
+
+
+
+
+
+async function downloadSong(arrSourcelinks) {
+  for await (const [i, sourcelink] of arrSourcelinks.entries()) {
+    client.getSongInfo(sourcelink)
+    .then(async songInfo => {
+
+      const filepath = `${songsTags[i].artist} - ${songsTags[i].title}`
+
+      const stream = await songInfo.downloadProgressive();
+      const writer = stream.pipe(fs.createWriteStream("./music/" + filepath + ".mp3"));
+
+      writer.on("finish", () => {
+        console.log("save song number: " + (i+1))
+
+        download(songsTags[i].image, "./music/temp/" + filepath + ".jpg", () => {
+          songsTags[i].image = "./music/temp/" + filepath + ".jpg";
+          NodeID3.write(songsTags[i], "./music/" + filepath + ".mp3", function(err) { }); //wywolanie funkcji zapisu z node-id3
+          console.log("tags song: " + (i+1))
+          
+        })
+      
+
+      });
     })
     .catch(console.error);
-});
+  }
+}
+
+const download = (url, path, callback) => {
+  request.head(url, (err, res, body) => {
+    request(url)
+      .pipe(fs.createWriteStream(path))
+      .on("close", callback)
+    
+  })
+}
+
+
+
+
+
+
 
 
 app.post('/download', urlencodedParser, (req, res) => {
 
-  client.getSongInfo(globalSourcelink)  //wywolanie scrapera znowu z linkiem, ale tym razem korzystac z globalnych danych, ktore zostaly zapisane w roucie wczeniej i zedytowane, chuj wie jak to dziala
-    .then(async globalSongInfo => {
-        const stream = await globalSongInfo.downloadProgressive();
-        const fileName = req.body.artist + " - " + req.body.title;
-        const writer = stream.pipe(fs.createWriteStream(`./music/${fileName}.mp3`));
-
-      //node-id3 section
-      //writing tags
-        const filebuffer = Buffer.from("Some Buffer of a (mp3) file") //chuj wie co to
-        const filepath = `./music/${fileName}.mp3`
-        const tags = {
-            title: req.body.title,
-            artist: req.body.artist,
-            album: req.body.soundcloud,
-            composer: req.body.spotify,
-            genre: req.body.youtube,
-            subtitle: req.body.sourcelink,
-        }
-
-        writer.on("finish", () => {
-        console.log("Finished writing song!")
-        NodeID3.write(tags, filepath, function(err) { }); //wywolanie funkcji zapisu z node-id3
-        res.json(req.body); //render json po edycji danych na poprzednim roucie
-        });
-    })
-    .catch(console.error);
+  async function rend() {
+    await updateSongsTags(songsTags, req.body)
+    await downloadSong(sourcelinks);
+    // console.log("ready")
     
-  // res.json(req.body)
+    res.json(req.body)
+    console.log("complete!");
+  }
+
+  rend();    
 });
 
 app.listen(port);
