f7406cc3210423c23f4cd057db790d8debba4d3c
ddbrn <ppq0qqp@gmail.com>
2024-02-01 23:35:27 +0100

    style: remove comments

--- public/public.js
+++ public/public.js
@@ -1,33 +1,8 @@
-// console.log("dzialam");
 let index = 1;
-
 function addRow() {
-    // if(document.getElementsByName(`sourcelink${index}`).innerHTML == "")
-    // {
-    //     console.log("tak");
-    // }
     let li = document.createElement('li');
-    // li.setAttribute('id', `${index}`);
-
-        document.getElementById("dynamic-list")
-        .appendChild(li)
-        // .innerHTML = `<input type="text" class="form-control sourcelink" name="sourcelink${index}">`
-        .innerHTML = `<input type="text" class="form-control sourcelink" name="sourcelink">`
-
-    
-        index++;
-
-        // let temp = index.toString();
-    // console.log(document.getElementById("'" + index + "'").innerHTML)
-    // console.log(index.toString());
-
-    
-
-    
-
-
-
-    // console.log("dzialam");
-
-
+    document.getElementById("dynamic-list")
+    .appendChild(li)
+    .innerHTML = `<input type="text" class="form-control sourcelink" name="sourcelink">`
+    index++;
 }
